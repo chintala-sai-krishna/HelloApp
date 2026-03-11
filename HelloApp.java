24fb2ff894fa2feb18e94ab4169d564bf2ec1f73
chintala-saikrishna <chintalasaikrishna2007@gmail.com>
2026-03-12 02:27:56 +0530

    UC1 Display Hello World

---
+++ HelloApp.java
@@ -0,0 +1,15 @@
+/**
+ * HelloApp
+ * UC1: Display Hello World
+ * 
+ * @author Sai
+ * @version 1.0
+ */
+
+public class HelloApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
